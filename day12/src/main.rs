d037ec647e5a36c25917566ea3ce4478f17bd46b
Jasper de Vries <jasper.devries@the-future-group.com>
2022-12-13 14:23:02 +0100

    Day 12 cleanup

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use nom::bytes::complete::is_a;
-use nom::character::complete::line_ending;
-use nom::combinator::map;
-use nom::multi::separated_list1;
-use nom::IResult;
+use nom::{
+    bytes::complete::is_a, character::complete::line_ending, combinator::map,
+    multi::separated_list1, IResult,
+};
 use pathfinding::prelude::bfs;
 
 const DATA: &str = include_str!("input.txt");
@@ -25,8 +24,8 @@ fn main() -> Result<()> {
 }
 
 fn part_one(input: &[Vec<u8>]) -> usize {
-    let start = find_single(input, b'S');
-    let goal = find_single(input, b'E');
+    let start = find_all(input, b'S')[0];
+    let goal = find_all(input, b'E')[0];
 
     let result = bfs(&start, |p| p.next_steps(input), |p| *p == goal).unwrap();
 
@@ -34,7 +33,7 @@ fn part_one(input: &[Vec<u8>]) -> usize {
 }
 
 fn part_two(input: &[Vec<u8>]) -> usize {
-    let goal = find_single(input, b'E');
+    let goal = find_all(input, b'E')[0];
     let starting_points = find_all(input, b'a');
 
     starting_points
@@ -44,16 +43,6 @@ fn part_two(input: &[Vec<u8>]) -> usize {
         .min()
         .unwrap()
         - 1
-}
-
-fn find_single(input: &[Vec<u8>], single: u8) -> Coord {
-    input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, h)| ((x, y), h)))
-        .find(|(_, h)| **h == single)
-        .map(|((x, y), _)| Coord(x, y))
-        .unwrap()
 }
 
 fn find_all(input: &[Vec<u8>], single: u8) -> Vec<Coord> {
@@ -76,49 +65,44 @@ struct Coord(usize, usize);
 
 impl Coord {
     fn next_steps(&self, input: &[Vec<u8>]) -> Vec<Coord> {
-        let mut next_steps: Vec<Coord> = vec![];
-        let current_height = get_height(input, self);
+        let width = input.get(0).unwrap().len() - 1;
+        let height = input.len() - 1;
+
+        let mut next_steps = vec![];
 
         // up
-        if self.1 > 0 {
-            let new_coord = Coord(self.0, self.1 - 1);
-            let new_height = get_height(input, &new_coord);
-            if Self::can_move(current_height, new_height) {
-                next_steps.push(new_coord);
-            }
-        }
-
+        self.calculate_step(&mut next_steps, input, |s| s.1 > 0, self.0, self.1 - 1);
         // down
-        if self.1 < input.len() - 1 {
-            let new_coord = Coord(self.0, self.1 + 1);
-            let new_height = get_height(input, &new_coord);
-            if Self::can_move(current_height, new_height) {
-                next_steps.push(new_coord);
-            }
-        }
-
+        self.calculate_step(&mut next_steps, input, |s| s.1 < height, self.0, self.1 + 1);
         // left
-        if self.0 > 0 {
-            let new_coord = Coord(self.0 - 1, self.1);
-            let new_height = get_height(input, &new_coord);
-            if Self::can_move(current_height, new_height) {
-                next_steps.push(new_coord);
-            }
-        }
-
+        self.calculate_step(&mut next_steps, input, |s| s.0 > 0, self.0 - 1, self.1);
         // right
-        if self.0 < input.get(0).unwrap().len() - 1 {
-            let new_coord = Coord(self.0 + 1, self.1);
-            let new_height = get_height(input, &new_coord);
-            if Self::can_move(current_height, new_height) {
-                next_steps.push(new_coord);
-            }
-        }
+        self.calculate_step(&mut next_steps, input, |s| s.0 < width, self.0 + 1, self.1);
 
         next_steps
     }
 
-    pub fn can_move(current_height: &u8, new_height: &u8) -> bool {
+    fn calculate_step<F>(
+        &self,
+        next_steps: &mut Vec<Coord>,
+        input: &[Vec<u8>],
+        expr: F,
+        x: usize,
+        y: usize,
+    ) where
+        F: Fn(&Coord) -> bool,
+    {
+        if expr(self) {
+            let new_coord = Coord(x, y);
+            let current_height = get_height(input, self);
+            let new_height = get_height(input, &new_coord);
+            if Self::can_move(current_height, new_height) {
+                next_steps.push(new_coord);
+            }
+        }
+    }
+
+    fn can_move(current_height: &u8, new_height: &u8) -> bool {
         (*new_height == b'E' && (*current_height == b'z' || *current_height == b'y'))
             || (*new_height == b'a' && *current_height == b'S')
             || (*new_height != b'E' && *new_height <= current_height + 1)
@@ -148,121 +132,8 @@ fn parse_input(input: &'static str) -> Result<Vec<Vec<u8>>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test_case::test_case;
 
     const TESTDATA: &str = include_str!("test.txt");
-
-    #[test_case(b'S', b'a')]
-    #[test_case(b'a', b'b')]
-    #[test_case(b'b', b'c')]
-    #[test_case(b'c', b'd')]
-    #[test_case(b'd', b'e')]
-    #[test_case(b'e', b'f')]
-    #[test_case(b'f', b'g')]
-    #[test_case(b'g', b'h')]
-    #[test_case(b'h', b'i')]
-    #[test_case(b'i', b'j')]
-    #[test_case(b'j', b'k')]
-    #[test_case(b'k', b'l')]
-    #[test_case(b'l', b'm')]
-    #[test_case(b'm', b'n')]
-    #[test_case(b'n', b'o')]
-    #[test_case(b'o', b'p')]
-    #[test_case(b'p', b'q')]
-    #[test_case(b'q', b'r')]
-    #[test_case(b'r', b's')]
-    #[test_case(b's', b't')]
-    #[test_case(b't', b'u')]
-    #[test_case(b'u', b'v')]
-    #[test_case(b'v', b'w')]
-    #[test_case(b'w', b'x')]
-    #[test_case(b'x', b'y')]
-    #[test_case(b'y', b'z')]
-    #[test_case(b'y', b'E')]
-    #[test_case(b'z', b'E')]
-    #[test_case(b'b', b'a')]
-    #[test_case(b'c', b'b')]
-    #[test_case(b'd', b'c')]
-    #[test_case(b'e', b'd')]
-    #[test_case(b'f', b'e')]
-    #[test_case(b'g', b'f')]
-    #[test_case(b'h', b'g')]
-    #[test_case(b'i', b'h')]
-    #[test_case(b'j', b'i')]
-    #[test_case(b'k', b'j')]
-    #[test_case(b'l', b'k')]
-    #[test_case(b'm', b'l')]
-    #[test_case(b'n', b'm')]
-    #[test_case(b'o', b'n')]
-    #[test_case(b'p', b'o')]
-    #[test_case(b'q', b'p')]
-    #[test_case(b'r', b'q')]
-    #[test_case(b's', b'r')]
-    #[test_case(b't', b's')]
-    #[test_case(b'u', b't')]
-    #[test_case(b'v', b'u')]
-    #[test_case(b'w', b'v')]
-    #[test_case(b'x', b'w')]
-    #[test_case(b'y', b'x')]
-    #[test_case(b'z', b'a')]
-    #[test_case(b'z', b'b')]
-    #[test_case(b'z', b'c')]
-    #[test_case(b'z', b'd')]
-    #[test_case(b'z', b'f')]
-    #[test_case(b'z', b'g')]
-    #[test_case(b'z', b'h')]
-    #[test_case(b'z', b'i')]
-    #[test_case(b'z', b'j')]
-    #[test_case(b'z', b'k')]
-    #[test_case(b'z', b'l')]
-    #[test_case(b'z', b'm')]
-    #[test_case(b'z', b'n')]
-    #[test_case(b'z', b'o')]
-    #[test_case(b'z', b'p')]
-    #[test_case(b'z', b'q')]
-    #[test_case(b'z', b'r')]
-    #[test_case(b'z', b's')]
-    #[test_case(b'z', b't')]
-    #[test_case(b'z', b'u')]
-    #[test_case(b'z', b'v')]
-    #[test_case(b'z', b'w')]
-    #[test_case(b'z', b'x')]
-    #[test_case(b'z', b'y')]
-    #[test_case(b'z', b'z')]
-    #[ignore]
-    fn test_part_one_coord(current_height: u8, new_height: u8) {
-        assert!(Coord::can_move(&current_height, &new_height));
-    }
-
-    #[test_case(b'S', b'b')]
-    #[test_case(b'a', b'c')]
-    #[test_case(b'b', b'd')]
-    #[test_case(b'c', b'e')]
-    #[test_case(b'd', b'f')]
-    #[test_case(b'e', b'g')]
-    #[test_case(b'f', b'h')]
-    #[test_case(b'g', b'i')]
-    #[test_case(b'h', b'j')]
-    #[test_case(b'i', b'k')]
-    #[test_case(b'j', b'l')]
-    #[test_case(b'k', b'm')]
-    #[test_case(b'l', b'n')]
-    #[test_case(b'm', b'o')]
-    #[test_case(b'n', b'p')]
-    #[test_case(b'o', b'q')]
-    #[test_case(b'p', b'r')]
-    #[test_case(b'q', b's')]
-    #[test_case(b'r', b't')]
-    #[test_case(b's', b'u')]
-    #[test_case(b't', b'v')]
-    #[test_case(b'u', b'w')]
-    #[test_case(b'v', b'x')]
-    #[test_case(b'w', b'y')]
-    #[test_case(b'x', b'z')]
-    #[ignore]
-    fn test_part_one_coord_not(current_height: u8, new_height: u8) {
-        assert!(!Coord::can_move(&current_height, &new_height));
-    }
 
     #[test]
     fn test_part_one_testdata() -> Result<()> {
@@ -278,11 +149,6 @@ mod tests {
         Ok(())
     }
 
-    #[test_case(b'E', b'z')]
-    fn test_part_two_coord(current_height: u8, new_height: u8) {
-        assert!(Coord::can_move_down(&current_height, &new_height));
-    }
-
     #[test]
     fn test_part_two_testdata() -> Result<()> {
         assert_eq!(29, part_two(&parse_input(TESTDATA)?));
@@ -292,7 +158,7 @@ mod tests {
 
     #[test]
     fn test_part_two() -> Result<()> {
-        assert_eq!(0, part_two(&parse_input(DATA)?));
+        assert_eq!(418, part_two(&parse_input(DATA)?));
 
         Ok(())
     }
